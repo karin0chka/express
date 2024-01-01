548377d9ee898a43c30a348c8dac6944ae4cda00
Karina <karina.kvych@gmail.com>
2024-01-01 19:06:14 +0000

    add

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,24 @@ const router = express.Router()
 
 //static routes first
 router.get('/', (req,res) =>{
+    console.log(req.query.name)
     res.send('User list')
 })
 router.get('/new',(req,res)=>{
-    res.send('User New Form')
+    res.render("users/new")
 })
 
 router.post('/',(req,res) =>{
-    res.send('Create User')
+    const isValid = true
+    if(isValid){
+        users.push({firstName: req.body.firstName})
+        res.redirect(`/users/${users.length - 1}`)
+    }else{
+        console.log("Error")
+        res.render('users/new', {firstName:req.body.firstName})
+    }
+   console.log(req.body.firstName)
+   res.send("Hi")
 })
 
 router.route("/:id").get((req,res) => {
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
